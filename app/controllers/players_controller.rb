8c9ba672c102881496a0af611466b6ad4d3bafe7
Jakub Arnold <darthdeus@gmail.com>
2011-10-10 19:08:06 +0200

    fixed player lookup

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -27,16 +27,14 @@ class PlayersController < ApplicationController
     unless @player
       flash[:alert] = "Player with given name isn't registered for the current tournament. Please contact our staff at channel 'SCV Rush' 30 minutes before the tournament starts."
       redirect_to :action => 'index'
-    end
-    
-    if @player.checked?
-      flash[:alert] = "You are already checked in."
     else
-      @player.checked = true
-      @player.save!
-      flash[:notice] = "You have successfully checked in."
+      if @player.checked?
+        flash[:alert] = "You are already checked in."
+      else
+        @player.checked = true
+        @player.save!
+        flash[:notice] = "You have successfully checked in."
+      end
     end
-
-    redirect_to :action => 'index'
   end
 end
